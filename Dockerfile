3ba65eebcfebb86995a4d215da324b5c28c99463
Nedko Hristov <nedelcho.hristov2001@gmail.com>
2022-09-07 00:03:17 +0300

    Node.js

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
-# Using prebaked official alpine image (size of 49MB)
-FROM node:current-alpine3.16
-LABEL org.opencontainers.image.authors="nedko.hristov@severalclouds.com"
+FROM node:erbium-alpine3.12
 WORKDIR /app
 COPY package*.json ./
+COPY yarn.lock ./
+RUN yarn
+COPY . .
+EXPOSE 80
+CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = new express();
 
 app.get("/", (req, res) =>
   res.send({
-    status: "Success",
-    message: "The GitHub to AWS ECR then to AWS AppRunner is working",
+    status: "✅ success",
+    message: "proton is awesome, so is copilot! ❤️",
   })
 );
 
-// app.listen(80, () => console.log("server listening on 80"));
+app.listen(80, () => console.log("server listening on 80"));
